a1c0dd080b8d0923ed8798ac6a0b154887713b7c
Abhi007-glitch <abhinav619000@gmail.com>
2022-09-18 14:59:24 +0530

    deal with CORS

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+var cors = require('cors');
 const FoodUserModel = require("./model/userModel");
 const ReviewModel = require("./model/reviewModel");
 const PlanModel = require("./model/planModel");
 // token name is -> JWT & mechanism -> cookies
+
 
 const cookieParser = require("cookie-parser");
  
@@ -12,6 +14,7 @@ const authRouter = require("./routes/authRoutes");
 const planRouter = require("./routes/planRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 
+app.use(cors()); // to deal with cross origin resource sharing (CROS) issue
 app.use(cookieParser());
 app.use(express.json());// define which kind of data is accepted
 
@@ -19,7 +22,6 @@ app.use(express.json());// define which kind of data is accepted
 app.use("/api/v1/user",userRouter);
 app.use('/api/v1/auth',authRouter); // now authRouter will represent '/api/v1/auth'
 app.use("/api/v1/plan",planRouter);
-
 app.use("/api/v1/review",reviewRouter );
 
 
